be4d4df0cc82080259bb3fa97ce80f5b98a22c68
[Sandeep] <sandeep977244@gmail.com>
2026-05-24 09:15:15 +0530

    Refactor code structure for improved readability and maintainability

---
+++ List_In_STL.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<list>
+#include<iterator>
+using namespace std;
+
+void printList(list<int>ll){
+
+    list<int>::iterator itr;
+
+    for(itr = ll.begin(); itr != ll.end(); itr++){
+        cout<<(*itr)<<"->";
+    }
+
+    cout<<"NULL"<<endl;
+
+}
+
+int main(){
+
+    list<int>ll;
+
+
+    ll.push_front(0);
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.push_back(4);
+
+    printList(ll);
+
+    cout<<ll.size()<<endl;
+
+    ll.pop_front();
+    ll.pop_back();
+
+    list<int>::iterator itr;
+
+    itr = ll.begin();
+    advance(itr,4);
+
+    ll.insert(itr,99);
+
+    printList(ll);
+    return 0;
+}
