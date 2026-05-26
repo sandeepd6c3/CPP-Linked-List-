feed4922e17b1fb843284a78e8c7b645a2f4cc90
[Sandeep] <sandeep977244@gmail.com>
2026-05-26 06:59:04 +0530

    Doubly Linked list Push_front function

---
+++ Doubly_LL.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+	public:
+	int data ;
+	Node* next;
+	Node* prev;
+
+	Node(int val){
+		data = val;
+		prev = next = NULL;
+	}
+};
+	class DoublyList{
+	public:
+	Node* head;
+	Node* tail;
+
+	DoublyList(){
+		head = tail = NULL;
+	}
+
+void push_front(int val){
+   Node* newnode = new Node(val);
+
+   if(head == NULL){
+	head = tail = newnode;
+   }
+   else{
+	newnode->next = head;
+	head->prev = newnode;
+	head = newnode;
+   }
+
+	}
+	
+	void print_ll(){
+		Node* temp = head;
+		
+		while(temp != NULL)
+		{
+			cout<<temp->data<<"->";
+			temp = temp->next;
+		}
+		cout<<"NULL";
+	}
+
+	};
+
+
+
+int main()
+{
+	
+	DoublyList dll;
+
+	dll.push_front(4);
+	dll.push_front(3);
+	dll.push_front(2);
+	dll.push_front(1);
+	dll.push_front(0);
+
+dll.print_ll();
+	return 0;
+}
